aa61466c7265538ae4cf68ac022dd756ddcf6a8b
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-08 13:42:36 -0800

    Updated Connections

--- db/conn.js
+++ db/conn.js
@@ -4,13 +4,12 @@ require("dotenv").config();
 const sequelize = new Sequelize(process.env.POSTGRESQL_DATABASE);
 
 // Test Connection Function
-
 async function testConnection() {
   try {
     await sequelize.authenticate({ logging: false });
     console.log("Connection has been established successfully!");
   } catch (error) {
-    console.log("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", error);
   }
 }
 
